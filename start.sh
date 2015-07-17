a1a32dc67888466b889d06c8904de85fe0d03c89
Roman <roman.atachiants@gmail.com>
2015-07-17 20:18:18 +0100

    bugfix

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 # First, we need to resolve the IP address of the seed node
 seed=$(/usr/bin/dig +short aerospike | /usr/bin/tail -1)
-/bin/sed -i -e 's/SEED_NODE/$seed/g' /etc/aerospike/aerospike.conf
-echo 'Aerospike seed node: $seed'
+/bin/sed -i -e "s/SEED_NODE/$seed/g" /etc/aerospike/aerospike.conf
+echo "Aerospike seed node: $seed"
 
 # Start aerospike
 /usr/bin/asd --foreground
